c896b6d84352d3507cd713156accf8aebe1ae3bc
Aleksei-Uzin <aleksei.uzin@gmail.com>
2026-01-20 00:01:31 +0300

    6 kyu: Can you keep a secret

---
+++ Fundamentals/6-kyu-Can-you-keep-a-secret.js
@@ -0,0 +1,17 @@
+/**
+ * Can you keep a secret?
+ * https://www.codewars.com/kata/5351b35ebaeb67f9110012d2/train/javascript
+ *
+ */
+
+function createSecretHolder(secret) {
+  return {
+    getSecret() {
+      return secret
+    },
+
+    setSecret(newSecret) {
+      secret = newSecret
+    },
+  }
+}
